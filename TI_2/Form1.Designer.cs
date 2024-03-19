b86950cf42ef0500609d522cbb0decbbba67b409
EvgenSavenok <eugen.savenok@gmail.com>
2024-03-19 17:14:13 +0300

    1

--- TI_2/Form1.Designer.cs
+++ TI_2/Form1.Designer.cs
@@ -33,7 +33,11 @@
       this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.startStateTB = new System.Windows.Forms.TextBox();
       this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.keyDataGridView = new System.Windows.Forms.DataGridView();
       this.menuStrip1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.keyDataGridView)).BeginInit();
       this.SuspendLayout();
       // 
       // menuStrip1
@@ -66,6 +70,7 @@
       this.startStateTB.Name = "startStateTB";
       this.startStateTB.Size = new System.Drawing.Size(325, 34);
       this.startStateTB.TabIndex = 1;
+      this.startStateTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.startStateTB_KeyPress);
       // 
       // label1
       // 
@@ -76,22 +81,61 @@
       this.label1.TabIndex = 2;
       this.label1.Text = "Начальное состояние регистра:";
       // 
-      // MainForm
+      // label2
+      // 
+      this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.Location = new System.Drawing.Point(12, 154);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(100, 23);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "Ключ:";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.textBox1.Location = new System.Drawing.Point(12, 203);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(325, 34);
+      this.textBox1.TabIndex = 4;
+      // 
+      // keyDataGridView
+      // 
+      this.keyDataGridView.AllowUserToAddRows = false;
+      this.keyDataGridView.AllowUserToDeleteRows = false;
+      this.keyDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.keyDataGridView.Location = new System.Drawing.Point(12, 279);
+      this.keyDataGridView.Name = "keyDataGridView";
+      this.keyDataGridView.ReadOnly = true;
+      this.keyDataGridView.RowTemplate.Height = 24;
+      this.keyDataGridView.Size = new System.Drawing.Size(776, 275);
+      this.keyDataGridView.TabIndex = 5;
+      // 
+      // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.ClientSize = new System.Drawing.Size(800, 716);
+      this.Controls.Add(this.keyDataGridView);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label2);
       this.Controls.Add(this.label1);
       this.Controls.Add(this.startStateTB);
       this.Controls.Add(this.menuStrip1);
       this.MainMenuStrip = this.menuStrip1;
-      this.Name = "MainForm";
+      this.Name = "Form1";
       this.Text = "ТИ_2";
+      this.Load += new System.EventHandler(this.Form1_Load);
       this.menuStrip1.ResumeLayout(false);
       this.menuStrip1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.keyDataGridView)).EndInit();
       this.ResumeLayout(false);
       this.PerformLayout();
     }
+
+    private System.Windows.Forms.DataGridView keyDataGridView;
+
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox textBox1;
 
     private System.Windows.Forms.TextBox startStateTB;
     private System.Windows.Forms.Label label1;
--- TI_2/Form1.cs
+++ TI_2/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TI_2
@@ -15,6 +8,32 @@ namespace TI_2
     public Form1()
     {
       InitializeComponent();
+      
+    }
+
+    private void startStateTB_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      if (e.KeyChar != '0' && e.KeyChar != '1' && !char.IsControl(e.KeyChar))
+      {
+        e.Handled = true;
+      }
+      if (e.KeyChar == 22)
+        e.Handled = true;
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      startStateTB.ContextMenu = new ContextMenu();
+      keyDataGridView.ColumnHeadersVisible = false; 
+      keyDataGridView.RowHeadersVisible = false;
+      keyDataGridView.Columns.Add("Column1", "Column1 Header");
+      keyDataGridView.Columns.Add("Column2", "Column2 Header");
+      keyDataGridView.Columns.Add("Column3", "Column3 Header");
+
+      // Добавляем строки с данными
+      keyDataGridView.Rows.Add("Value1", "Value2", "Value3");
+      keyDataGridView.Rows.Add("Value4", "Value5", "Value6");
+      keyDataGridView.Rows.Add("Value7", "Value8", "Value9");
     }
   }
 }
