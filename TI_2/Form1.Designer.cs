d466ec682533febc7dfa99c06d7666898c542c53
EvgenSavenok <eugen.savenok@gmail.com>
2024-03-19 21:36:25 +0300

    1

--- TI_2/Form1.Designer.cs
+++ TI_2/Form1.Designer.cs
@@ -29,34 +29,47 @@
     private void InitializeComponent()
     {
       this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-      this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.startStateTB = new System.Windows.Forms.TextBox();
       this.label1 = new System.Windows.Forms.Label();
       this.label2 = new System.Windows.Forms.Label();
       this.keyDataGridView = new System.Windows.Forms.DataGridView();
-      this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.cipherBtn = new System.Windows.Forms.Button();
       this.dechipherBtn = new System.Windows.Forms.Button();
+      this.getKeyBtn = new System.Windows.Forms.Button();
+      this.sourceDataGrid = new System.Windows.Forms.DataGridView();
+      this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+      this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
       this.menuStrip1.SuspendLayout();
       ((System.ComponentModel.ISupportInitialize)(this.keyDataGridView)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.sourceDataGrid)).BeginInit();
       this.SuspendLayout();
       // 
       // menuStrip1
       // 
-      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.файлToolStripMenuItem });
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.ToolStripMenuItem });
       this.menuStrip1.Location = new System.Drawing.Point(0, 0);
       this.menuStrip1.Name = "menuStrip1";
       this.menuStrip1.Size = new System.Drawing.Size(800, 28);
       this.menuStrip1.TabIndex = 0;
       this.menuStrip1.Text = "menuStrip1";
       // 
-      // файлToolStripMenuItem
+      // ToolStripMenuItem
       // 
-      this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { this.открытьToolStripMenuItem, this.сохранитьToolStripMenuItem });
-      this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-      this.файлToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
-      this.файлToolStripMenuItem.Text = "Файл";
+      this.ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { this.открытьToolStripMenuItem, this.сохранитьToolStripMenuItem });
+      this.ToolStripMenuItem.Name = "ToolStripMenuItem";
+      this.ToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+      this.ToolStripMenuItem.Text = "Файл";
+      // 
+      // открытьToolStripMenuItem
+      // 
+      this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+      this.открытьToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+      this.открытьToolStripMenuItem.Size = new System.Drawing.Size(202, 24);
+      this.открытьToolStripMenuItem.Text = "Открыть";
+      this.открытьToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
       // 
       // сохранитьToolStripMenuItem
       // 
@@ -64,6 +77,7 @@
       this.сохранитьToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
       this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(202, 24);
       this.сохранитьToolStripMenuItem.Text = "Сохранить";
+      this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
       // 
       // startStateTB
       // 
@@ -87,7 +101,7 @@
       // label2
       // 
       this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label2.Location = new System.Drawing.Point(12, 215);
+      this.label2.Location = new System.Drawing.Point(12, 433);
       this.label2.Name = "label2";
       this.label2.Size = new System.Drawing.Size(100, 23);
       this.label2.TabIndex = 3;
@@ -100,24 +114,18 @@
       this.keyDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
       this.keyDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
       this.keyDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-      this.keyDataGridView.Location = new System.Drawing.Point(12, 255);
+      this.keyDataGridView.Location = new System.Drawing.Point(12, 477);
       this.keyDataGridView.Name = "keyDataGridView";
       this.keyDataGridView.ReadOnly = true;
       this.keyDataGridView.RowTemplate.Height = 24;
       this.keyDataGridView.Size = new System.Drawing.Size(776, 275);
       this.keyDataGridView.TabIndex = 5;
       // 
-      // открытьToolStripMenuItem
-      // 
-      this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
-      this.открытьToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-      this.открытьToolStripMenuItem.Size = new System.Drawing.Size(202, 24);
-      this.открытьToolStripMenuItem.Text = "Открыть";
-      // 
       // cipherBtn
       // 
+      this.cipherBtn.Enabled = false;
       this.cipherBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.cipherBtn.Location = new System.Drawing.Point(12, 144);
+      this.cipherBtn.Location = new System.Drawing.Point(12, 771);
       this.cipherBtn.Name = "cipherBtn";
       this.cipherBtn.Size = new System.Drawing.Size(193, 55);
       this.cipherBtn.TabIndex = 6;
@@ -126,19 +134,55 @@
       // 
       // dechipherBtn
       // 
+      this.dechipherBtn.Enabled = false;
       this.dechipherBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.dechipherBtn.Location = new System.Drawing.Point(245, 144);
+      this.dechipherBtn.Location = new System.Drawing.Point(570, 771);
       this.dechipherBtn.Name = "dechipherBtn";
       this.dechipherBtn.Size = new System.Drawing.Size(218, 55);
       this.dechipherBtn.TabIndex = 7;
       this.dechipherBtn.Text = "Дешифровать";
       this.dechipherBtn.UseVisualStyleBackColor = true;
       // 
+      // getKeyBtn
+      // 
+      this.getKeyBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.getKeyBtn.Location = new System.Drawing.Point(438, 70);
+      this.getKeyBtn.Name = "getKeyBtn";
+      this.getKeyBtn.Size = new System.Drawing.Size(218, 55);
+      this.getKeyBtn.TabIndex = 8;
+      this.getKeyBtn.Text = "Получить ключ";
+      this.getKeyBtn.UseVisualStyleBackColor = true;
+      this.getKeyBtn.Click += new System.EventHandler(this.getKeyBtn_Click);
+      // 
+      // sourceDataGrid
+      // 
+      this.sourceDataGrid.AllowUserToAddRows = false;
+      this.sourceDataGrid.AllowUserToDeleteRows = false;
+      this.sourceDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+      this.sourceDataGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+      this.sourceDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.sourceDataGrid.Location = new System.Drawing.Point(12, 144);
+      this.sourceDataGrid.Name = "sourceDataGrid";
+      this.sourceDataGrid.ReadOnly = true;
+      this.sourceDataGrid.RowTemplate.Height = 24;
+      this.sourceDataGrid.Size = new System.Drawing.Size(776, 275);
+      this.sourceDataGrid.TabIndex = 9;
+      // 
+      // openFileDialog
+      // 
+      this.openFileDialog.FileName = "openFileDialog";
+      // 
+      // saveFileDialog
+      // 
+      this.saveFileDialog.Filter = "All files (*.*)|*.*";
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 716);
+      this.ClientSize = new System.Drawing.Size(800, 909);
+      this.Controls.Add(this.sourceDataGrid);
+      this.Controls.Add(this.getKeyBtn);
       this.Controls.Add(this.dechipherBtn);
       this.Controls.Add(this.cipherBtn);
       this.Controls.Add(this.keyDataGridView);
@@ -148,14 +192,23 @@
       this.Controls.Add(this.menuStrip1);
       this.MainMenuStrip = this.menuStrip1;
       this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "ТИ_2";
       this.Load += new System.EventHandler(this.Form1_Load);
       this.menuStrip1.ResumeLayout(false);
       this.menuStrip1.PerformLayout();
       ((System.ComponentModel.ISupportInitialize)(this.keyDataGridView)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.sourceDataGrid)).EndInit();
       this.ResumeLayout(false);
       this.PerformLayout();
     }
+
+    private System.Windows.Forms.OpenFileDialog openFileDialog;
+    private System.Windows.Forms.SaveFileDialog saveFileDialog;
+
+    private System.Windows.Forms.DataGridView sourceDataGrid;
+
+    private System.Windows.Forms.Button getKeyBtn;
 
     private System.Windows.Forms.Button cipherBtn;
     private System.Windows.Forms.Button dechipherBtn;
@@ -170,7 +223,7 @@
     private System.Windows.Forms.Label label1;
 
     private System.Windows.Forms.MenuStrip menuStrip1;
-    private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem;
     private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
 
     #endregion
--- TI_2/Form1.cs
+++ TI_2/Form1.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
 
 namespace TI_2
 {
   public partial class Form1 : Form
   {
+    private string _startStateOfRegister;
+    private byte[] _fileBytes;
+    private string _fileExtension;
+    List<List<int>> _bitsList = new List<List<int>>();
     public Form1()
     {
       InitializeComponent();
@@ -21,35 +27,117 @@ namespace TI_2
         e.Handled = true;
     }
 
-    private void FillDataGrid(string data)
-    { 
-      const int numOfCols = 27;
-      int curNumOfCols;
-      if (data.Length > numOfCols)
-        curNumOfCols = numOfCols;
-      else
-        curNumOfCols = data.Length;
-      for (int i = 0; i < curNumOfCols; i++)
-        keyDataGridView.Columns.Add("", "");
-      int rowIndex = 0;
-      for (int i = 0; i < data.Length; i++)
-      {
-        if (i % curNumOfCols == 0)
-        {
-          rowIndex++;
-          keyDataGridView.Rows.Add();
-        }
-        keyDataGridView.Rows[rowIndex - 1].Cells[i % curNumOfCols].Value = data[i].ToString();
-      }
-    }
-
     private void Form1_Load(object sender, EventArgs e)
     {
       startStateTB.ContextMenu = new ContextMenu();
       keyDataGridView.ColumnHeadersVisible = false;
       keyDataGridView.RowHeadersVisible = false;
-      string data = "1010101010001010100010101011001";
-      FillDataGrid(data);
+      sourceDataGrid.ColumnHeadersVisible = false;
+      sourceDataGrid.RowHeadersVisible = false;
+    }
+    private void ReadFromFile(string path)
+    {
+      using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read)) {
+        using (var reader = new BinaryReader(stream)) {
+          try
+          {
+            _fileBytes = reader.ReadBytes((int) stream.Length);
+            _fileExtension = Path.GetExtension(path);
+          }
+          catch (Exception)
+          {
+            Console.WriteLine("Не удалось прочитать файл!");
+          }
+        }          
+      }
+    }
+    
+    private void FillSourceDataGrid(byte[] data, DataGridView grid)
+    { 
+      const int maxBytesToShow = 100; 
+      const int numOfCols = 8;
+      int curNumOfCols = Math.Min(data.Length, numOfCols);  
+      for (int i = 0; i < curNumOfCols; i++)
+        grid.Columns.Add("", "");
+      int rowIndex = 0;
+      for (int i = 0; i < data.Length; i++)
+      {
+        if (rowIndex > maxBytesToShow)
+          return;
+        List<int> bits = new List<int>();
+        for (int j = 0; j < numOfCols; j++)
+        {
+          int bit = (data[i] >> j) & 1;
+          bits.Add(bit);
+          if (grid.Rows.Count <= rowIndex)
+            grid.Rows.Add();
+          grid.Rows[rowIndex].Cells[j].Value = bit;
+        }
+        _bitsList.Add(bits);
+        rowIndex++;
+      }
+    }
+    private void HandleOpenedFile()
+    {
+      string openedFileName;
+      DialogResult result = openFileDialog.ShowDialog();
+      if ((result == DialogResult.OK) && (result != DialogResult.Cancel))
+      {
+        openedFileName = openFileDialog.FileName;
+        ReadFromFile(openedFileName);
+      }
+    }
+    private void ToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      HandleOpenedFile();
+      FillSourceDataGrid(_fileBytes, sourceDataGrid);
+    }
+
+    private byte[] ConvertToByteArray()
+    {
+      List<byte> byteArray = new List<byte>();
+      foreach (List<int> row in _bitsList)
+      {
+        byte currentByte = 0;
+        for (int i = 0; i < row.Count; i++)
+          currentByte |= (byte)(row[i] << i);
+        byteArray.Add(currentByte);
+      }
+      return byteArray.ToArray();
+    }
+    private void SaveBytesAsFile(string path)
+    {
+      using (FileStream fs = new FileStream(path, FileMode.Create))
+      {
+        using (BinaryWriter bw = new BinaryWriter(fs))
+        {
+          byte[] byteArray = ConvertToByteArray();
+          bw.Write(byteArray);
+        }
+      }
+    }
+    private void HandleSavedFile()
+    {
+      string savedFileName;
+      DialogResult result = saveFileDialog.ShowDialog();
+      if ((result == DialogResult.OK) && (result != DialogResult.Cancel))
+      {
+        savedFileName = saveFileDialog.FileName + _fileExtension;
+        SaveBytesAsFile(savedFileName);
+      }
+    }
+    private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      HandleSavedFile();
+    }
+    private void GenerateKey()
+    {
+      _startStateOfRegister = startStateTB.Text;
+      
+    }
+    private void getKeyBtn_Click(object sender, EventArgs e)
+    {
+      GenerateKey();
     }
   }
 }
